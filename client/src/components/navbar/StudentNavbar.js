addafb4d8d39506496590420ce567ca07ab9c5e2
Nethu <21103670@usc.edu.ph>
2024-06-01 22:10:25 +0800

    Update StudentNavbar.js

--- client/src/components/navbar/StudentNavbar.js
+++ client/src/components/navbar/StudentNavbar.js
@@ -4,11 +4,9 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="w-screen flex flex-row justify-between items-center bg-white px-5 py-4 border-b-[5px] border-custom-grey2">
-      <Link to='/user' className="font-['Open_Sans'] font-bold text-2xl">ONLINE SARI-SARI</Link>
+      <Link to='/user' className="font-bold text-2xl">LOGO</Link>
       <div className="flex items-center font-['Barlow_Semi_Condensed'] text-xl">
-        <Link to='/user' className="cursor-pointer ms-8 hover:text-gray-500">DASHBOARD</Link>
-        <Link to='/user/products' className="cursor-pointer ms-8 hover:text-gray-500">PRODUCTS</Link>
-        <div className="cursor-pointer border border-black px-4 py-1 ms-8 hover:bg-custom-grey1">LOGOUT</div>      
+        <Link to='/user' className="cursor-pointer ms-8 hover:text-gray-500">Home</Link>    
       </div>
     </nav>
   )
