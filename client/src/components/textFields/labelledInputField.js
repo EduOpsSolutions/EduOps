2d28d48c28fbdd806d33c34904a57de6a54c1a6c
Marck-road <109259291+Marck-road@users.noreply.github.com>
2024-07-18 18:44:12 +0800

    Sign up Page tweaking
    > Added comments in Sign-up Page
    > Fixed issue where not labelled textfields' labels would not move up enough once text is placed
    > Added select courses to enroll dropdown kay nakalimtan nako
    > Added small right padding to labelled input fields
    > Added back button to sign up page
    > Added back button component

--- client/src/components/textFields/labelledInputField.js
+++ client/src/components/textFields/labelledInputField.js
@@ -6,7 +6,7 @@ const LabelledInputField = ({ name, id, label, type = "text", required = true , 
             <label htmlFor={id} class="block mb-2 text-sm font-medium dark:text-white">
                 {label}
             </label>
-            <input type={type} name={name} id={id} className="block py-2.5 px-0 w-full text-sm pl-2 bg-white text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-dark-red focus:outline-none focus:ring-0 focus:border-dark-red peer" placeholder={placeholder} required={required}/>
+            <input type={type} name={name} id={id} className="block py-2.5 px-2 w-full text-sm pl-2 bg-white text-gray-900 bg-transparent border-2 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-dark-red focus:outline-none focus:ring-0 focus:border-dark-red peer" placeholder={placeholder} required={required}/>
         </div>
     );
 }
--- client/src/pages/signUp.js
+++ client/src/pages/signUp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import bg_image from '../assets/bg_7.jpg';
+import BackButton from '../components/buttons/backButton';
 import SmallButton from '../components/buttons/smallButton';
 import SignUpNav from '../components/navbars/signUpNav';
 import FileUploadButton from '../components/textFields/fileUploadButton';
@@ -13,9 +14,10 @@ function SignUp() {
 
     // Function to navigate to login page
     const navigateToLogin = () => {
-        navigate("/login");
+        navigate("/");
     };
 
+    // Add any missing options
     const civilStatusOptions = [
         { value: 'single', label: 'Single' },
         { value: 'married', label: 'Married' },
@@ -32,83 +34,99 @@ function SignUp() {
         { value: 'other', label: 'Other'}
     ];
 
+    // Replace with back-end logic for all avail course options
+    const courseOptions = [
+        { value: 'A1', label: 'A1'},
+        { value: 'A2', label: 'A2'},
+        { value: 'B1', label: 'B1'},
+        { value: 'B2', label: 'B2'},
+        { value: 'C1', label: 'C1'},
+        { value: 'C2', label: 'C2'}
+    ];
+
+
     return (
         <section className='flex justify-center items-center bg-white-yellow-tone bg-center bg-cover bg-no-repeat bg-blend-multiply' style={{ backgroundImage: `url(${bg_image})`, minHeight: '100vh', backgroundPosition: '100% 35%',}}>
             <SignUpNav />
-                <form className="max-w-full mx-auto bg-white-yellow-tone w-11/12 px-8 py-4 mt-32 mb-12 flex flex-col">
-
-
-                <h1 className='text-center text-2xl font-bold'>Enrollment Form</h1>
-                <p className='italic mb-2'>Items with (*) are required fields</p>
-                <div className="grid md:grid-cols-3 md:gap-6">
-                    <NotLabelledInputField name="first_name" id="first_name" label="First name*" type="text" required={true} />
-                    <NotLabelledInputField name="middle_name" id="middle_name" label="Middle name*" type="text" required={true} />
-                    <NotLabelledInputField name="last_name" id="last_name" label="Last name*" type="text" required={true} />
-                </div>
-                <div class="grid md:grid-cols-4 md:gap-6">
-                    <LabelledInputField name="extensions" id="extensions" label="Extensions" type="text" required={false} placeholder="Jr., Sr. III" />
-                    <div class="grid md:grid-cols-2 md:gap-6">
-                        <LabelledInputField name="honorrific" id="honorrific" label="Honorrific*" type="text" required={true} placeholder="Mr., Ms" />   
-                        <LabelledInputField name="sex" id="sex" label="Sex*" type="text" required={true} placeholder="M/F" />
+            <div className="relative max-w-full mx-auto bg-white-yellow-tone w-11/12 px-8 py-4 mt-32 mb-12 flex flex-col">
+                <BackButton onClick={navigateToLogin} />
+                <form className="px-8 py-4 flex flex-col">
+                    <h1 className='text-center text-3xl font-bold'>Enrollment Form</h1>
+                    <p className='italic mb-5'>Items with (*) are required fields</p>
+                    <div className="grid md:grid-cols-3 md:gap-6">
+                        <NotLabelledInputField name="first_name" id="first_name" label="First name*" type="text" required={true} />
+                        <NotLabelledInputField name="middle_name" id="middle_name" label="Middle name*" type="text" required={true} />
+                        <NotLabelledInputField name="last_name" id="last_name" label="Last name*" type="text" required={true} />
                     </div>
-                    <LabelledInputField name="birthdate" id="birthdate" label="Birth Date*" type="date" required={true} />
-                    <SelectField name="civil_status" id="civil_status" label="Civil Status*" required={true} options={civilStatusOptions} />              
-                </div>
-                <div class="grid md:grid-cols-3 md:gap-6">
-                    <div className="col-span-2">
-                        <LabelledInputField name="address" id="address" label="Current Address*" type="text" required={true} placeholder="Street, Barangay, City, Province, Zip Code" />
-                    </div>
-                    <div className="col-span-1">
-                        <SelectField name="referred_by" id="referred_by" label="Referred By*" required={true} options={referredByOptions} />                      
-                    </div>
-                </div>
-                <div class="grid md:grid-cols-2 md:gap-6">
-                    <LabelledInputField name="contact_number" id="contact_number" label="Contact Number*" type="tel" required={true} placeholder="+63 9xxxxxxxxxx" />
-                    <LabelledInputField name="alt_contact_number" id="alt_contact_number" label="Alternate Contact Number" type="tel" required={false} placeholder="+63 9xxxxxxxxxx" />
-                </div>
-                <div class="grid md:grid-cols-2 md:gap-6">
-                    <LabelledInputField name="preferred_email" id="preferred_email" label="Preferred Email Address*" type="email" required={true} placeholder="johndoe@gmail.com" />
-                    <LabelledInputField name="alt_email" id="alt_email" label="Alternate Email Address" type="email" required={false} placeholder="example@gmail.com" />
-                </div>
-                <div class="grid md:grid-cols-3 md:gap-6">
-                    <LabelledInputField name="mother_name" id="mother_name" label="Mother's Maiden Full Name" type="text" required={false} placeholder="" />
-                    <div class="grid md:grid-cols-2 md:gap-6">
-                        <div className="flex flex-col items-center">
-                            <label htmlFor="deceased" className="mb-2 text-sm font-medium dark:text-white">
-                            Deceased
-                            </label>
-                            <input type="checkbox" name="mother_deceased" id="mother_deceased" className="peer mt-3 text-dark-red focus:border-dark-red focus:ring-dark-red-2" />
+                    <div class="grid md:grid-cols-4 md:gap-6">
+                        <LabelledInputField name="extensions" id="extensions" label="Extensions" type="text" required={false} placeholder="Jr., Sr. III" />
+                        <div class="grid md:grid-cols-2 md:gap-6">
+                            <LabelledInputField name="honorrific" id="honorrific" label="Honorrific*" type="text" required={true} placeholder="Mr., Ms" />
+                            <LabelledInputField name="sex" id="sex" label="Sex*" type="text" required={true} placeholder="M/F" />
                         </div>
-                        <LabelledInputField name="mother_contact_number" id="mother_contact_number" label="Contact Number" type="tel" required={false} placeholder="+63 9xxxxxxxxxx" />
+                        <LabelledInputField name="birthdate" id="birthdate" label="Birth Date*" type="date" required={true} />
+                        <SelectField name="civil_status" id="civil_status" label="Civil Status*" required={true} options={civilStatusOptions} />
                     </div>
-                </div>
-                <div class="grid md:grid-cols-3 md:gap-6">
-                    <LabelledInputField name="father_name" id="father_name" label="Father's Full Name" type="text" required={false} placeholder="" />
-                    <div class="grid md:grid-cols-2 md:gap-6">
-                        <div className="flex justify-center items-center">
-                            <input type="checkbox" name="father_deceased" id="father_deceased" className="peer text-dark-red focus:border-dark-red focus:ring-dark-red-2" />
+                    <div class="grid md:grid-cols-3 md:gap-6">
+                        <div className="col-span-2">
+                            <LabelledInputField name="address" id="address" label="Current Address*" type="text" required={true} placeholder="Street, Barangay, City, Province, Zip Code" />
                         </div>
-                        <LabelledInputField name="father_contact_number" id="father_contact_number" label="Contact Number" type="tel" required={false} placeholder="+63 9xxxxxxxxxx" />
-                    </div>
-                </div>
-                <div class="grid md:grid-cols-3 md:gap-6">
-                    <LabelledInputField name="guardian_name" id="guardian_name" label="Guardian's Full Name" type="text" required={false} placeholder="" />
-                    <div class="grid md:grid-cols-2 md:gap-6">
-                        <div className="flex justify-center items-center">
-                            <input type="checkbox" name="guardian_deceased" id="guardian_deceased" className="peer text-dark-red focus:border-dark-red focus:ring-dark-red-2" />
+                        <div className="col-span-1">
+                            <SelectField name="referred_by" id="referred_by" label="Referred By*" required={true} options={referredByOptions} />
                         </div>
-                        <LabelledInputField name="mother_contact_number" id="mother_contact_number" label="Contact Number" type="tel" required={false} placeholder="+63 9xxxxxxxxxx" />
                     </div>
-                </div>
-                <FileUploadButton label="Upload Valid ID (front and back)" id="valid_id" placeholder="Choose File" ariaDescribedBy="valid_id_help" />
-                <FileUploadButton label="Upload 2X2 ID Photo (white background)" id="2x2_id" placeholder="Choose File" ariaDescribedBy="2x2_id_help" />
-
-                <SmallButton onClick={""}>
-                    Proceed
-                </SmallButton>                
-                
-            </form>
-
+                    <div class="grid md:grid-cols-2 md:gap-6">
+                        <LabelledInputField name="contact_number" id="contact_number" label="Contact Number*" type="tel" required={true} placeholder="+63 9xxxxxxxxxx" />
+                        <LabelledInputField name="alt_contact_number" id="alt_contact_number" label="Alternate Contact Number" type="tel" required={false} placeholder="+63 9xxxxxxxxxx" />
+                    </div>
+                    <div class="grid md:grid-cols-2 md:gap-6">
+                        <LabelledInputField name="preferred_email" id="preferred_email" label="Preferred Email Address*" type="email" required={true} placeholder="johndoe@gmail.com" />
+                        <LabelledInputField name="alt_email" id="alt_email" label="Alternate Email Address" type="email" required={false} placeholder="example@gmail.com" />
+                    </div>
+                    <div class="grid md:grid-cols-3 md:gap-6">
+                        <LabelledInputField name="mother_name" id="mother_name" label="Mother's Maiden Full Name" type="text" required={false} placeholder="" />
+                        <div class="grid md:grid-cols-2 md:gap-6">
+                            <div className="flex flex-col items-center">
+                                <label htmlFor="deceased" className="mb-2 text-sm font-medium dark:text-white">
+                                Deceased?
+                                </label>
+                                <input type="checkbox" name="mother_deceased" id="mother_deceased" className="peer mt-3 text-dark-red focus:border-dark-red focus:ring-dark-red-2" />
+                            </div>
+                            <LabelledInputField name="mother_contact_number" id="mother_contact_number" label="Contact Number" type="tel" required={false} placeholder="+63 9xxxxxxxxxx" />
+                        </div>
+                    </div>
+                    <div class="grid md:grid-cols-3 md:gap-6">
+                        <LabelledInputField name="father_name" id="father_name" label="Father's Full Name" type="text" required={false} placeholder="" />
+                        <div class="grid md:grid-cols-2 md:gap-6">
+                            <div className="flex justify-center items-center">
+                                <input type="checkbox" name="father_deceased" id="father_deceased" className="peer text-dark-red focus:border-dark-red focus:ring-dark-red-2" />
+                            </div>
+                            <LabelledInputField name="father_contact_number" id="father_contact_number" label="Contact Number" type="tel" required={false} placeholder="+63 9xxxxxxxxxx" />
+                        </div>
+                    </div>
+                    <div class="grid md:grid-cols-3 md:gap-6">
+                        <LabelledInputField name="guardian_name" id="guardian_name" label="Guardian's Full Name" type="text" required={false} placeholder="" />
+                        <div class="grid md:grid-cols-2 md:gap-6">
+                            <div className="flex justify-center items-center">
+                                <input type="checkbox" name="guardian_deceased" id="guardian_deceased" className="peer text-dark-red focus:border-dark-red focus:ring-dark-red-2" />
+                            </div>
+                            <LabelledInputField name="mother_contact_number" id="mother_contact_number" label="Contact Number" type="tel" required={false} placeholder="+63 9xxxxxxxxxx" />
+                        </div>
+                    </div>
+                    <div class="grid md:grid-cols-3 md:gap-6">
+                        <div>
+                            <SelectField name="courses_to_enroll" id="courses_to_enroll" label="Select Course(s) to Enroll*" required={true} options={courseOptions} />
+                        </div>
+                    </div>
+                    {/* Need to find way how to change the add file button's color to red from gray */}
+                    <FileUploadButton label="Upload Valid ID (front and back)" id="valid_id" placeholder="Choose File" ariaDescribedBy="valid_id_help" />
+                    <FileUploadButton label="Upload 2X2 ID Photo (white background)" id="2x2_id" placeholder="Choose File" ariaDescribedBy="2x2_id_help" />
+                    {/* Add Onclick function to go to enrollment */}
+                    <SmallButton onClick={""}>
+                        Proceed
+                    </SmallButton>
+                </form>
+            </div>
         </section>
     );
 
