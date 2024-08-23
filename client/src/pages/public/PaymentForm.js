ea7679dbfea427cf096e00c1fdea25efd376548d
chrstysrss <21102824@usc.edu.ph>
2024-08-23 23:04:30 +0800

    Updated payment form
    Button is still not responsive:<

--- client/src/pages/public/PaymentForm.js
+++ client/src/pages/public/PaymentForm.js
@@ -3,6 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import Bg_image from '../../assets/images/Bg2.png';
 import BackButton from '../../components/buttons/BackButton';
 import SignUpNav from '../../components/navbars/SignUpNav';
+import LabelledInputField from '../../components/textFields/LabelledInputField';
+import SelectField from '../../components/textFields/SelectField';
+import SmallButton from '../../components/buttons/SmallButton';
+
 
 
 function PaymentForm() {
@@ -12,11 +16,44 @@ function PaymentForm() {
         navigate("/");
     };
 
+    const feesOptions = [
+        { value: 'course_fee', label: 'Course Fee' },
+        { value: 'book_fee', label: 'Book Fee' },
+        { value: 'document_fee', label: 'Document Fee' }
+    ];
+
+
     return (
         <section className='flex justify-start bg-white-yellow-tone bg-center bg-cover bg-repeat bg-blend-multiply' style={{ backgroundImage: `url(${Bg_image})`, minHeight: '100vh'}}>
             <SignUpNav/>
-            <div className="flex flex-col relative max-w-full mx-auto bg-white-yellow-tone w-11/12 px-8 py-4 mt-32 mb-12">
+            <div className="flex flex-col relative max-w-full mx-auto bg-white-yellow-tone w-11/12 px-8 py-4 mt-32 mb-auto rounded-xl border-2 border-dark-red shadow-2xl md:min-h-[580px] h-auto">
                 <BackButton onClick={navigateToLogin} />
+                <form className="px-8 py-4 flex flex-col">
+                <h1 className='text-center text-3xl font-bold mt-5 -mb-2'>Maya Payment Form</h1>
+                <hr className='my-8  border-dark-red'/>
+                <p className='-mt-4 mb-5 font-semibold'>Please enter valid information.</p>   
+
+                <div className="grid md:grid-cols-3 md:gap-10">
+                    <LabelledInputField name="first_name" id="first_name" label="First Name" type="text" required={true} placeholder="First Name" />
+                    <LabelledInputField name="middle_name" id="middle_name" label="Middle Name" type="text" required={true} placeholder="Middle Name"/>
+                    <LabelledInputField name="last_name" id="last_name" label="Last Name" type="text" required={true} placeholder="Last Name"/>
+                </div>
+                <div className="grid md:grid-cols-3 md:gap-10">
+                    <LabelledInputField name="email_address" id="email_address" label="Email Address" type="text" required={true} placeholder="johndoe@gmail.com" />
+                    <LabelledInputField name="phone_number" id="phone_number" label="Phone Number" type="tel" required={true} placeholder="+63 9xxxxxxxxxx"/>
+                </div>
+                <hr className='mt-2 mb-8 border-dark-red'/>
+                <p className='-mt-4 mb-5 font-semibold'>Type of Fees.</p>
+                <div className="grid md:grid-cols-3 md:gap-10">
+                    <SelectField name="fee" id="fee" label="Fee" required={true} options={feesOptions} />  
+                    <LabelledInputField name="amount" id="amount" label="Amount" type="text" required={true} placeholder="0.00" />
+                </div>
+                </form>
+            </div> 
+            <div className='fixed bottom-8 right-12'>
+                <SmallButton onClick={'Maya'}>
+                Submit
+                </SmallButton>
             </div>
         </section>
     )
