3eafefe21ab3177a5a4e7476c05d1b368146a6c4
Mark David Calzada <markdavidcalzada@gmail.com>
2024-08-23 19:45:30 +0800

    Fixed box not expanding

--- client/src/pages/public/PaymentForm.js
+++ client/src/pages/public/PaymentForm.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Bg_image from '../../assets/images/Bg2.png';
-import SignUpNav from '../../components/navbars/SignUpNav';
-import BackButton from '../../components/buttons/BackButton';
 import { useNavigate } from 'react-router-dom';
+import Bg_image from '../../assets/images/Bg2.png';
+import BackButton from '../../components/buttons/BackButton';
+import SignUpNav from '../../components/navbars/SignUpNav';
 
 
 function PaymentForm() {
@@ -13,11 +13,11 @@ function PaymentForm() {
     };
 
     return (
-        <section className='flex items-start justify-start flex-col bg-white-yellow-tone bg-center bg-cover bg-repeat bg-blend-multiply' style={{ backgroundImage: `url(${Bg_image})`, minHeight: '100vh'}}>
-        <SignUpNav/>
-        <div className="relative max-w-full mx-auto bg-white-yellow-tone w-11/12 px-8 py-4 mt-32 mb-12 flex flex-col">
-            <BackButton onClick={navigateToLogin} />
-        </div>
+        <section className='flex justify-start bg-white-yellow-tone bg-center bg-cover bg-repeat bg-blend-multiply' style={{ backgroundImage: `url(${Bg_image})`, minHeight: '100vh'}}>
+            <SignUpNav/>
+            <div className="flex flex-col relative max-w-full mx-auto bg-white-yellow-tone w-11/12 px-8 py-4 mt-32 mb-12">
+                <BackButton onClick={navigateToLogin} />
+            </div>
         </section>
     )
 }
