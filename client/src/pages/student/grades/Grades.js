4e5f5265d53dc5edd6fd5737ec94049d5faac7c8
Angelou Sereño <115084999+DanRed22@users.noreply.github.com>
2024-11-12 16:15:39 +0800

    Revert "Added Temporary View Grade Details Modal"

--- client/src/pages/student/grades/Grades.js
+++ client/src/pages/student/grades/Grades.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import Bg_image from '../../../assets/images/Bg2.png';
-import GradeDetailsModal from "../../../components/modals/GradeDetailsModal";
 import GradeNotReadyModal from '../../../components/modals/GradeNotReadyModal';
 
 function Grades() {
     const [grade_not_ready_modal, setGradeNotReadyModal] = useState(false);
-    const [grade_details_modal, setGradeDetailsModal] = useState(false);
+
 
     return (
         <section className='flex flex-col items-start justify-start bg-white-yellow-tone bg-center bg-cover bg-repeat bg-blend-multiply' style={{ backgroundImage: `url(${Bg_image})`, minHeight: '100vh'}}>
@@ -35,7 +34,7 @@ function Grades() {
                         <tbody>
                             {/* Todo: Insert backend to display courses enrolled */}
                             <tr class=" dark:bg-gray-500 dark:border-gray-700 border-b border-b-gray-500">
-                                <td class="px-6 py-2 font-medium text-gray-900 dark:text-white">
+                                <td scope="row" class="px-6 py-2 font-medium text-gray-900 dark:text-white">
                                     B1 Aligemein Course
                                 </td>
                                 <td class="px-6 py-2 text-center">
@@ -52,20 +51,20 @@ function Grades() {
                             {/* TODO: create a modal that displays the grades of the student with an ingrained file
                             viewer */}
                             <tr class=" dark:bg-gray-800 border-b border-b-gray-500">
-                                <td class="px-6 py-2 font-medium text-gray-900 dark:text-white">
+                                <td scope="row" class="px-6 py-2 font-medium text-gray-900 dark:text-white">
                                     A2 German Basic Course
                                 </td>
                                 <td class="px-6 py-2 text-center">
                                     PASS
                                 </td>
                                 <td class="px-6 py-2 flex justify-center items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer" onClick={() => setGradeDetailsModal(true)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                                     </svg>
                                 </td>
                             </tr>
                             <tr class=" dark:bg-gray-800 border-b border-b-gray-500">
-                                <td class="px-6 py-2 font-medium text-gray-900 dark:text-white">
+                                <td scope="row" class="px-6 py-2 font-medium text-gray-900 dark:text-white">
                                     A1 German Basic Course
                                 </td>
                                 <td class="px-6 py-2 text-center">
@@ -78,7 +77,7 @@ function Grades() {
                                 </td>
                             </tr>
                             <tr class="dark:bg-gray-800 border-b border-b-gray-500">
-                                <td class="px-6 py-2 font-medium text-gray-900 dark:text-white">
+                                <td scope="row" class="px-6 py-2 font-medium text-gray-900 dark:text-white">
                                     A1 German Basic Course
                                 </td>
                                 <td class="px-6 py-2 text-center">
@@ -97,12 +96,6 @@ function Grades() {
                         grade_not_ready_modal={grade_not_ready_modal}
                         setGradeNotReadyModal={setGradeNotReadyModal}
                     />
-
-                    <GradeDetailsModal
-                        grade_details_modal={grade_details_modal}
-                        setGradeDetailsModal={setGradeDetailsModal}
-                    />
-
                 </div>
 
             </div>
