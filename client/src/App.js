a01a051d64661f06d942f7a72bc21d108f14a0c8
DanRed22 <angeloushatchitsolutions@gmail.com>
2025-05-26 19:40:46 +0800

    Update .gitignore, clean up package-lock.json, and refactor App.js for improved readability

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,51 @@
-import React from 'react';
-import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import './App.css';
+import React from "react";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import "./App.css";
 
 // Import axios instance
-import axios from './utils/axios';
+import axios from "./utils/axios";
 
 /*Utility Pages*/
 import Assets from "./pages/Assets";
 
 /* General Pages */
-import PrivacyPolicy from './pages/legal/PrivacyPolicy';
-import Terms from './pages/legal/Terms';
-import Login from './pages/public/Login';
-import NotFound from './pages/public/NotFound';
-import PaymentForm from './pages/public/PaymentForm';
-import Profile from './pages/public/Profile';
-import RedirectPage from './pages/public/RedirectPage';
-import SignUp from './pages/public/SignUp';
+import PrivacyPolicy from "./pages/legal/PrivacyPolicy";
+import Terms from "./pages/legal/Terms";
+import Login from "./pages/public/Login";
+import NotFound from "./pages/public/NotFound";
+import PaymentForm from "./pages/public/PaymentForm";
+import Profile from "./pages/public/Profile";
+import RedirectPage from "./pages/public/RedirectPage";
+import SignUp from "./pages/public/SignUp";
 
 /* Student Pages */
-import StudentLayout from './components/layout/StudentLayout';
-import Documents from './pages/student/Documents';
-import Enrollment from './pages/student/Enrollment';
-import Grades from './pages/student/Grades';
-import Home from './pages/student/Home';
-import Profile from './pages/student/Profile';
-import StudentSchedule from './pages/student/StudentSchedule';
-import StudyLoad from './pages/student/StudyLoad';
-import Ledger from './pages/student/Ledger';
-import Assessment from './pages/student/Assessment';
-
+import StudentLayout from "./components/layout/StudentLayout";
+import Documents from "./pages/student/Documents";
+import Enrollment from "./pages/student/Enrollment";
+import Grades from "./pages/student/Grades";
+import Home from "./pages/student/Home";
+import StudentSchedule from "./pages/student/StudentSchedule";
+import StudyLoad from "./pages/student/StudyLoad";
+import Ledger from "./pages/student/Ledger";
+import Assessment from "./pages/student/Assessment";
 
 /* Teacher Pages */
-import TeacherLayout from './components/layout/TeacherLayout';
-import TeacherGrades from './pages/teacher/Grades';
-import TeacherHome from './pages/teacher/Home';
-import TeachingLoad from './pages/teacher/TeachingLoad';
+import TeacherLayout from "./components/layout/TeacherLayout";
+import TeacherGrades from "./pages/teacher/Grades";
+import TeacherHome from "./pages/teacher/Home";
+import TeachingLoad from "./pages/teacher/TeachingLoad";
 
 /* Admin Pages */
-import AdminLayout from './components/layout/AdminLayout';
-import CourseManagement from './pages/admin/CourseManagement';
-import AdminHome from './pages/admin/Home';
-import EnrollmentRequests from './pages/admin/EnrollmentRequests';
-import EnrollmentPeriod from './pages/admin/EnrollmentPeriod';
+import AdminLayout from "./components/layout/AdminLayout";
+import CourseManagement from "./pages/admin/CourseManagement";
+import AdminHome from "./pages/admin/Home";
+import EnrollmentRequests from "./pages/admin/EnrollmentRequests";
+import EnrollmentPeriod from "./pages/admin/EnrollmentPeriod";
 
 function App() {
   return (
@@ -50,54 +53,49 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="login" element={<Login/>}/>
-          <Route path="assets" element={<Assets/>}/>
-          <Route path="signUp" element={<SignUp/>}/>
-          <Route path="paymentForm" element={<PaymentForm/>}/>
-          <Route path="redirectPage" element={<RedirectPage/>}/>
-          <Route path="sign-up" element={<SignUp/>}/>
-          <Route path="privacy-policy" element={<PrivacyPolicy/>}/>
-          <Route path="terms" element={<Terms/>}/>
-
+          <Route path="login" element={<Login />} />
+          <Route path="assets" element={<Assets />} />
+          <Route path="signUp" element={<SignUp />} />
+          <Route path="paymentForm" element={<PaymentForm />} />
+          <Route path="redirectPage" element={<RedirectPage />} />
+          <Route path="sign-up" element={<SignUp />} />
+          <Route path="privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="terms" element={<Terms />} />
 
           {/* Student Page Routes */}
           <Route path="student" element={<StudentLayout />}>
-            <Route index element={<Home />} />                            {/* localhost/student */}
-            <Route path="enrollment" element={<Enrollment/>}/>
-            <Route path="schedule" element={<StudentSchedule/>}/>
-            <Route path="profile" element={<Profile role="student"/>}/>
-            <Route path="studyLoad" element={<StudyLoad/>}/>
-            <Route path="grades"  element={<Grades />}/> 
-            <Route path="assessment" element={<Assessment/>}/>
-            <Route path="ledger" element={<Ledger/>}/>
-            <Route path="documents"  element={<Documents />}/> 
-            <Route path="profile" element={<Profile/>}/>
+            <Route index element={<Home />} /> {/* localhost/student */}
+            <Route path="enrollment" element={<Enrollment />} />
+            <Route path="schedule" element={<StudentSchedule />} />
+            <Route path="studyLoad" element={<StudyLoad />} />
+            <Route path="grades" element={<Grades />} />
+            <Route path="assessment" element={<Assessment />} />
+            <Route path="ledger" element={<Ledger />} />
+            <Route path="documents" element={<Documents />} />
+            <Route path="profile" element={<Profile />} />
           </Route>
 
           {/* Teacher Page Routes */}
           <Route path="teacher" element={<TeacherLayout />}>
-            <Route index element={<TeacherHome />} />   
-            <Route path="profile" element={<Profile role="teacher"/>}/>         
-            <Route path="grades"  element={<TeacherGrades />}/>
-            <Route path="teachingLoad"  element={<TeachingLoad />}/>                   
-            <Route path="documents"  element={<Documents />}/>
+            <Route index element={<TeacherHome />} />
+            <Route path="profile" element={<Profile role="teacher" />} />
+            <Route path="grades" element={<TeacherGrades />} />
+            <Route path="teachingLoad" element={<TeachingLoad />} />
+            <Route path="documents" element={<Documents />} />
           </Route>
 
           {/* Admin Page Routes */}
           <Route path="admin" element={<AdminLayout />}>
-            <Route index element={<AdminHome />} />  
-            <Route path="coursemanagement"  element={<CourseManagement />}/>  
-            <Route path="profile" element={<Profile role="admin"/>}/>  
-            <Route path="coursemanagement" element={<CourseManagement />}/>           
-            <Route path="enrollmentrequests" element={<EnrollmentRequests />}/>           
-            <Route path="enrollmentperiod" element={<EnrollmentPeriod />}/> 
+            <Route index element={<AdminHome />} />
+            <Route path="coursemanagement" element={<CourseManagement />} />
+            <Route path="profile" element={<Profile role="admin" />} />
+            <Route path="coursemanagement" element={<CourseManagement />} />
+            <Route path="enrollmentrequests" element={<EnrollmentRequests />} />
+            <Route path="enrollmentperiod" element={<EnrollmentPeriod />} />
           </Route>
-
-
 
           {/* Not Found Page */}
           <Route path="*" element={<NotFound />} />
-          
         </Routes>
       </Router>
     </div>
