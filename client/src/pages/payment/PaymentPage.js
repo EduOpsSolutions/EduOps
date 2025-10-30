7fc63e9d6393dcc69c6cd36438b173934bfd1028
chrstysrss <90500109+chrstysrss@users.noreply.github.com>
2025-10-30 22:41:04 +0800

    Update email assignment in PaymentPage to prioritize paymentEmail over user email
    - Modified the email assignment logic in PaymentPage.js to use payment.paymentEmail

--- client/src/pages/payment/PaymentPage.js
+++ client/src/pages/payment/PaymentPage.js
@@ -80,7 +80,7 @@ const PaymentPage = () => {
         studentId: user.studentId || user.userId || null,
         firstName: user.firstName || user.first_name || '',
         lastName: user.lastName || user.last_name || '',
-        email: user.email || ''
+        email: payment.paymentEmail || user.email || ''
       } : null;
 
       setPaymentData({ amount, description, studentInfo });
