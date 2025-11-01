1002103dfc5afa614ad1895e9049d54e8688eef9
chrstysrss <90500109+chrstysrss@users.noreply.github.com>
2025-11-01 16:25:44 +0800

    feat: Added custom loading in Student and Teacher Home pages

--- client/src/pages/student/Home.js
+++ client/src/pages/student/Home.js
@@ -2,9 +2,6 @@ import React, { useEffect } from 'react';
 import PostCard from '../../components/post/PostCard';
 import usePostsStore from '../../stores/postsStore';
 import Pagination from '../../components/common/Pagination';
-import Spinner from '../../components/common/Spinner';
-
-
 
 function Home() {
   const { getVisiblePosts, fetchPosts, isLoading } = usePostsStore();
@@ -27,7 +24,8 @@ function Home() {
     <div className="bg_custom bg-white-yellow-tone min-h-[calc(100vh-80px)] box-border flex flex-col py-4 sm:py-6 px-4 sm:px-8 md:px-12 lg:px-20">
       {isLoading ? (
         <div className="flex justify-center items-center py-12">
-          <Spinner size="lg" color="text-dark-red-2" message="Loading posts..." />
+          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+          <p className="ml-4 text-gray-600">Loading posts...</p>
         </div>
       ) : visiblePosts.length === 0 ? (
         <div className="text-center py-12">
