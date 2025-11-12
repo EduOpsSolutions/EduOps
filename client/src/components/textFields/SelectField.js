2f9f088bd29483fb462351235e5799ff4f4c6f0a
ReddyM20 <94691903+ReddyM20@users.noreply.github.com>
2025-11-13 02:03:14 +0800

    Fix autofill to explicitly be for document fee and auto-select document fee
    - Enhance SelectField component to support controlled value prop and update PaymentForm to clarify auto-fill logic for user details and document fees.

--- client/src/components/textFields/SelectField.js
+++ client/src/components/textFields/SelectField.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SelectField = ({ name, id, label, required, options, onChange, defaultValue, disabled }) => {
+const SelectField = ({ name, id, label, required, options, onChange, value, defaultValue, disabled }) => {
     return (
         <div className="relative z-0 w-full group mb-5 group">
             <label htmlFor={id} className="block mb-2 text-sm font-medium dark:text-gray-400">
@@ -11,7 +11,7 @@ const SelectField = ({ name, id, label, required, options, onChange, defaultValu
                 id={id}
                 className={`mt-2 py-2.5 bg-white border-2 border-gray-300 rounded-md text-gray-900 text-sm focus:ring-dark-red-2 focus:border-dark-red block w-full dark:bg-dark-red-5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark-red-2 dark:focus:border-dark-red-2 ${disabled ? 'bg-gray-200 cursor-not-allowed' : ''}`}
                 required={required}
-                defaultValue={defaultValue || ""}
+                value={value !== undefined ? value : defaultValue || ""}
                 onChange={onChange}
                 disabled={disabled}
             >
